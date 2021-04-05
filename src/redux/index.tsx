66e066535abd9a8ec00dc4f9f2614a82546fc71b
wangqi <wangqi908@hotmail.com>
2021-04-05 09:12:56 +0800

    数据持久化

--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,11 +1,23 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, Store, AnyAction } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './rootReducer'
 
 export type AppState = ReturnType<typeof rootReducer>
 
-export default createStore(
-  rootReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['todo']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(
+  persistedReducer,
   composeWithDevTools(applyMiddleware(thunk))
 )
+
+persistStore((store as unknown) as Store<any, AnyAction>)
+export default store
